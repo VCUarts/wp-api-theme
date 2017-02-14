b96cfaccba108664f85ecd1ce7829632d3a2133f
streetlamp <cody@codywhitby.net>
2017-02-14 16:20:47 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,20 @@
 <?php
+if (!function_exists('wp_rest_api_alter')){
+
+function wp_rest_api_alter() {
+  register_api_field( 'post',
+      'fields',
+      array(
+        'get_callback'    => function($data, $field, $request, $type){
+          if (function_exists('get_fields')) {
+            return get_fields($data['id']);
+          }
+          return [];
+        },
+        'update_callback' => null,
+        'schema'          => null,
+      )
+  );
+}
+add_action( 'rest_api_init', 'wp_rest_api_alter');
+}
